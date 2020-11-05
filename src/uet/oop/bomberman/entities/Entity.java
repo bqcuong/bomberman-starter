a16346fda9bc56792a9f319fe34ac2be9362e2e7
doanthihoaithu <hoaithuxt1998@gmail.com>
2020-11-05 14:02:12 +0700

    update Entity constructor, convert unit coordinate to real coordinate in Canvas

--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -8,24 +8,23 @@ import javafx.scene.paint.Color;
 import uet.oop.bomberman.graphics.Sprite;
 
 public abstract class Entity {
+    //Tọa độ X tính từ góc trái trên trong Canvas
     protected int x;
+
+    //Tọa độ Y tính từ góc trái trên trong Canvas
     protected int y;
+
     protected Image img;
 
-    public Entity( int x, int y, Image img) {
-        this.x = x;
-        this.y = y;
+    //Khởi tạo đối tượng, chuyển từ tọa độ đơn vị sang tọa độ trong canvas
+    public Entity( int xUnit, int yUnit, Image img) {
+        this.x = xUnit * Sprite.SCALED_SIZE;
+        this.y = yUnit * Sprite.SCALED_SIZE;
         this.img = img;
     }
 
     public void render(GraphicsContext gc) {
-//        SnapshotParameters params = new SnapshotParameters();
-//        params.setFill(Color.TRANSPARENT);
-//
-//        ImageView iv = new ImageView(img);
-//        Image base = iv.snapshot(params, null);
-
-        gc.drawImage(img, x * Sprite.SCALED_SIZE, y * Sprite.SCALED_SIZE);
+        gc.drawImage(img, x, y);
     }
     public abstract void update();
 }
