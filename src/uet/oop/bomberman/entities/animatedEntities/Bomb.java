daa9f2a8da0315730f76c2d0bf687278736102f1
manh <manhsuper171@gmail.com>
2021-11-10 22:53:34 +0700

    update with ExplosionController

--- src/uet/oop/bomberman/entities/animatedEntities/Bomb.java
+++ src/uet/oop/bomberman/entities/animatedEntities/Bomb.java
@@ -1,8 +1,5 @@
 package uet.oop.bomberman.entities.animatedEntities;
 
-import uet.oop.bomberman.BombermanGame;
-import uet.oop.bomberman.entities.Entity;
-import uet.oop.bomberman.entities.animatedEntities.flames.*;
 import uet.oop.bomberman.graphics.Sprite;
 
 public class Bomb extends AnimatedEntity{
@@ -23,114 +20,10 @@ public class Bomb extends AnimatedEntity{
      * and add it into entities */
 
     public void explode() {
-        explosion(new Explosion(xUnit, yUnit));
-
-        int leftUnit = xUnit - 1;
-        int rightUnit = xUnit + 1;
-        int topUnit = yUnit - 1;
-        int bottomUnit = yUnit + 1;
-        int leftLastUnit = xUnit - 2;
-        int rightLastUnit = xUnit + 2;
-        int topLastUnit = yUnit - 2;
-        int bottomLastUnit = yUnit + 2;
-
-        int left = leftUnit * Sprite.SCALED_SIZE;
-        int right = rightUnit * Sprite.SCALED_SIZE;
-        int top = topUnit * Sprite.SCALED_SIZE;
-        int bottom = bottomUnit * Sprite.SCALED_SIZE;
-        int leftLast = leftLastUnit * Sprite.SCALED_SIZE;
-        int rightLast = rightLastUnit * Sprite.SCALED_SIZE;
-        int topLast = topLastUnit * Sprite.SCALED_SIZE;
-        int bottomLast = bottomLastUnit * Sprite.SCALED_SIZE;
-
-        int xScaled = xUnit * Sprite.SCALED_SIZE;
-        int yScaled = yUnit * Sprite.SCALED_SIZE;
-
-        boolean leftCanExplode = true;
-        boolean rightCanExplode = true;
-        boolean topCanExplode = true;
-        boolean bottomCanExplode = true;
-        boolean leftLastCanExplode = true;
-        boolean rightLastCanExplode = true;
-        boolean topLastCanExplode = true;
-        boolean bottomLastCanExplode = true;
-
-        for (Entity element: BombermanGame.stillObjects) {
-            if (element.getClass().getTypeName()
-                .equals("uet.oop.bomberman.entities.staticEntities.Wall")) {
-                if (element.existOn(left, yScaled)) {
-                    leftCanExplode = false;
-                }
-                if (element.existOn(right, yScaled)) {
-                    rightCanExplode = false;
-                }
-                if (element.existOn(xScaled, top)) {
-                    topCanExplode = false;
-                }
-                if (element.existOn(xScaled, bottom)) {
-                    bottomCanExplode = false;
-                }
-                if (element.existOn(leftLast, yScaled)) {
-                    leftLastCanExplode = false;
-                }
-                if (element.existOn(rightLast, yScaled)) {
-                    rightLastCanExplode = false;
-                }
-                if (element.existOn(xScaled, topLast)) {
-                    topLastCanExplode = false;
-                }
-                if (element.existOn(xScaled, bottomLast)) {
-                    bottomLastCanExplode = false;
-                }
-            }
-        }
-        if (leftCanExplode) {
-            explosion(new Horizontal(leftUnit, yUnit));
-        }
-        if (rightCanExplode) {
-            explosion(new Horizontal(rightUnit, yUnit));
-        }
-        if (topCanExplode) {
-            explosion(new Vertical(xUnit, topUnit));
-        }
-        if (bottomCanExplode) {
-            explosion(new Vertical(xUnit, bottomUnit));
-        }
-        if (leftLastCanExplode) {
-            explosion(new LeftLast(leftLastUnit, yUnit));
-        }
-        if (rightLastCanExplode) {
-            explosion(new RightLast(rightLastUnit, yUnit));
-        }
-        if (topLastCanExplode) {
-            explosion(new TopLast(xUnit, topLastUnit));
-        }
-        if (bottomLastCanExplode) {
-            explosion(new DownLast(xUnit, bottomLastUnit));
-        }
-
-        // check bomber collide
-        for (Entity entity: BombermanGame.entities) {
-           if (entity.getClass().getTypeName().contains("Bomber")) {
-               if (entity.existOnSquare(x, y) ||
-                   entity.existOn(left, yScaled) ||
-                   entity.existOn(right, yScaled) ||
-                   entity.existOn(xScaled, top) ||
-                   entity.existOn(xScaled, bottom) ||
-                   entity.existOn(leftLast, yScaled) ||
-                   entity.existOn(rightLast, yScaled) ||
-                   entity.existOn(xScaled, topLast) ||
-                   entity.existOn(xScaled, bottomLast)) {
-                   ((Bomber) entity).setAlive(false);
-               }
-               break;
-           }
-        }
+        ExplosionController explosion = new ExplosionController(xUnit, yUnit);
+        explosion.begin();
     }
 
-    private void explosion(Explosion flame) {
-        BombermanGame.updateQueue.add(flame);
-    }
     @Override
     public void update() {
         animate();
@@ -141,7 +34,6 @@ public class Bomb extends AnimatedEntity{
                     animatedTime, 500_000_000).getFxImage();
         } else if (!exploded) {
             setVisible(false);
-//            explode(x, y);
             explode();
             exploded = true;
         }
