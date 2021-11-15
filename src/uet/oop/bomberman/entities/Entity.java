ff2ae10ddc1579f27067f58970d9bc2f71447e62
manh <manhsuper171@gmail.com>
2021-11-15 09:30:11 +0700

    update

--- src/uet/oop/bomberman/entities/Entity.java
+++ src/uet/oop/bomberman/entities/Entity.java
@@ -33,6 +33,14 @@ public abstract class Entity {
         return y;
     }
 
+    public int getxUnit() {
+        return xUnit;
+    }
+
+    public int getyUnit() {
+        return yUnit;
+    }
+
     //Khởi tạo đối tượng, chuyển từ tọa độ đơn vị sang tọa độ trong canvas
     public Entity( int xUnit, int yUnit, Image img) {
         this.xUnit = xUnit;
